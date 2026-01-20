82775ab690b6007228d6a3a9cda074263d234495
Benjamin Eakin <benjamin@example.com>
2026-01-20 13:09:49 -0500

    Add build env config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  env: {
+    SKIP_ENV_VALIDATION: process.env.SKIP_ENV_VALIDATION || "false",
+  },
 };
 
 export default nextConfig;
